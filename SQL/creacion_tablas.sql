4ceedee52ecc51e499a7b98e9d49d352bfafbad2
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2023-12-15 00:42:42 +0100

    .sql

--- SQL/creacion_tablas.sql
+++ SQL/creacion_tablas.sql
@@ -4,5 +4,14 @@ CREATE TABLE IF NOT EXISTS public.jubilados (
     apellido varchar(50) NULL,
     edad int NULL,
     años_tributados int NULL,
-    pension_anual int NULL
+    pension_anual int null,
+    geografico_id int null
+);
+
+CREATE table if not EXISTS public.geografico (
+	geografico_id int NULL,
+	localidad varchar NULL,
+	provincia varchar NULL,
+	comunidad_autonoma varchar NULL,
+	es_capital bool NULL
 );
