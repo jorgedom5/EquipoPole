fc97dfaa8fec2b66963a6eeabd3a5f7866b32760
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2023-12-15 11:18:20 +0100

    Update creacion_tablas.sql

--- SQL/creacion_tablas.sql
+++ SQL/creacion_tablas.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.jubilados (
-    jubilado_id int NULL,
+    jubilado_id SERIAL PRIMARY KEY,
     nombre varchar(50) NULL,
     apellido varchar(50) NULL,
     edad int NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS public.jubilados (
 );
 
 CREATE table if not EXISTS public.geografico (
-	geografico_id int NULL,
+	geografico_id SERIAL PRIMARY KEY,
 	localidad varchar NULL,
 	provincia varchar NULL,
 	comunidad_autonoma varchar NULL,
@@ -22,10 +22,11 @@ INSERT INTO public.jubilados (nombre, apellido, edad, años_tributados, pension_
 SELECT
     nombres.nombre,
     apellidos.apellido,
-    FLOOR(RANDOM() * (80 - 60 + 1) + 60) as edad,
-    FLOOR(RANDOM() * (40 - 20 + 1) + 20) as años_tributados,
-    FLOOR(RANDOM() * (50000 - 30000 + 1) + 30000) as pension_anual,
-    FLOOR(RANDOM() * 1000) as geografico_id
+    FLOOR(RANDOM() * (100 - 58 + 1) + 58) as edad, --Entre 58 y 100 años
+    FLOOR(RANDOM() * (42 - 15 + 1) + 15) as años_tributados, --Entre 15 y 42 años
+    FLOOR(RANDOM() * (42823 - 10963 + 1) + 10963) as pension_anual, --Pension minima anual (10963) y maxima (42823)
+    FLOOR(RANDOM() * 20) + 1 as geografico_id --Como aún no hay nada es hasta 20
 FROM unnest(ARRAY['Juan', 'María', 'Pedro', 'Ana', 'Carlos', 'Luis', 'Elena', 'Javier', 'Laura', 'Miguel', 'Isabel', 'Rosa', 'David', 'Sandra', 'Alberto', 'Patricia', 'Raúl', 'Carmen', 'Antonio', 'Beatriz', 'Francisco', 'Lorena', 'Diego', 'Monica']) as nombres(nombre),
      unnest(ARRAY['Gómez', 'Fernández', 'López', 'Rodríguez', 'Pérez', 'Martínez', 'González', 'Sánchez', 'Ramírez', 'Díaz', 'Hernández', 'Torres', 'Flores', 'Ortiz', 'Vargas', 'Ruiz', 'Molina', 'Jiménez', 'Moreno', 'Álvarez', 'Romero', 'Herrera', 'García', 'Lara']) as apellidos(apellido)
 LIMIT 5000;
+
