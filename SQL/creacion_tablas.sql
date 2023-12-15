4287bc4dc4c73d56e9d3c9c6fa3c4c768ed7a816
Jorge Dominguez <148787367+jorgedom5@users.noreply.github.com>
2023-12-15 01:02:10 +0100

    Update creacion_tablas.sql

--- SQL/creacion_tablas.sql
+++ SQL/creacion_tablas.sql
@@ -15,3 +15,17 @@ CREATE table if not EXISTS public.geografico (
 	comunidad_autonoma varchar NULL,
 	es_capital bool NULL
 );
+
+--PARA CREAR MUCHOS JUBILADOS (NO SE OPTIMIZAR LOS ARRAYS EN SQL)
+
+INSERT INTO public.jubilados (nombre, apellido, edad, años_tributados, pension_anual, geografico_id)
+SELECT
+    nombres.nombre,
+    apellidos.apellido,
+    FLOOR(RANDOM() * (80 - 60 + 1) + 60) as edad,
+    FLOOR(RANDOM() * (40 - 20 + 1) + 20) as años_tributados,
+    FLOOR(RANDOM() * (50000 - 30000 + 1) + 30000) as pension_anual,
+    FLOOR(RANDOM() * 1000) as geografico_id
+FROM unnest(ARRAY['Juan', 'María', 'Pedro', 'Ana', 'Carlos', 'Luis', 'Elena', 'Javier', 'Laura', 'Miguel', 'Isabel', 'Rosa', 'David', 'Sandra', 'Alberto', 'Patricia', 'Raúl', 'Carmen', 'Antonio', 'Beatriz', 'Francisco', 'Lorena', 'Diego', 'Monica']) as nombres(nombre),
+     unnest(ARRAY['Gómez', 'Fernández', 'López', 'Rodríguez', 'Pérez', 'Martínez', 'González', 'Sánchez', 'Ramírez', 'Díaz', 'Hernández', 'Torres', 'Flores', 'Ortiz', 'Vargas', 'Ruiz', 'Molina', 'Jiménez', 'Moreno', 'Álvarez', 'Romero', 'Herrera', 'García', 'Lara']) as apellidos(apellido)
+LIMIT 5000;
