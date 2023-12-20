c855acedadfdc322bb224ec87aa5999a2491cd6b
Jorge Dominguez <148787367+jorgedom5@users.noreply.github.com>
2023-12-20 16:59:18 +0100

    ADD jubilado_id

--- jubilados.py
+++ jubilados.py
@@ -7,15 +7,14 @@ import numpy as np
 fake = Faker('es_ES')  # Configura Faker para generar datos en español
 gender_detector = Detector()
 
-#FUNCIÓN PARA CREAR EDAD POR PORCENTAJES
-
+# FUNCIÓN PARA CREAR EDAD POR PORCENTAJES
 def generar_edad():
     prob = fake.random_int(1, 100)
-    if prob <= 31.03: #7.57; 31.03 (0.2439)
+    if prob <= 31.03:  # 7.57; 31.03 (0.2439)
         return fake.random_int(min=58, max=64)
-    elif prob <= 52.5:  #5.48; 22.47
+    elif prob <= 52.5:  # 5.48; 22.47
         return fake.random_int(min=65, max=69)
-    elif prob <= 70.55:  #4.72; 19.35
+    elif prob <= 70.55:  # 4.72; 19.35
         return fake.random_int(min=70, max=74)
     elif prob <= 85.5:  # 3.98; 16.32
         return fake.random_int(min=75, max=79)
@@ -25,22 +24,23 @@ def generar_edad():
         return fake.random_int(min=85, max=100)
 
 # AQUÍ PARA CREAR COLUMNAS
-def generar_datos():
+def generar_datos(jubilado_id):
     nombre_con_acentos = fake.first_name()
     nombre_sin_acentos = unidecode(nombre_con_acentos)
     genero = gender_detector.get_gender(nombre_sin_acentos)
     edad = generar_edad()
-    endeudamiento = fake.random_int(1, 100) <= 18 # Endeudamiento es True el 18% de los casos
+    endeudamiento = fake.random_int(1, 100) <= 18  # Endeudamiento es True el 18% de los casos
     return {
+        'jubilado_id': jubilado_id,
         'nombre': nombre_sin_acentos,
         'apellido': fake.last_name(),
         'genero': genero,
         'edad': edad,
         'endeudamiento': endeudamiento
     }
-    
-# 5000 filas de datos
-datos = [generar_datos() for _ in range(5000)]
+
+# 5000 filas de datos con 'jubilado_id'
+datos = [generar_datos(jubilado_id) for jubilado_id in range(1, 5001)]
 df = pd.DataFrame(datos)
 
 # Guardar el DataFrame en un archivo CSV
