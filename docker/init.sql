e9b41b0c82b40ffc9212ceac3ac2dec94216bfa5
Yael Larrosa Soler <148787445+Yael-Larrosa@users.noreply.github.com>
2023-12-21 17:27:45 +0100

    Update init.sql

--- docker/init.sql
+++ docker/init.sql
@@ -6,3 +6,37 @@ create table if not exists tipo_turismo(
 insert into tipo_turismo (tipo_turismo)
 values
 ('Capital de provincia'), ('Turismo de naturaleza'), ('Turismo cultural'),('Turismo de descanso')
+
+
+create table if not exists mes(
+	mes_id serial primary key,
+	mes varchar(50) not null
+)
+
+insert into mes (mes)
+values
+('Enero'),
+('Febrero'),
+('Marzo'),
+('Abril'),
+('Mayo'),
+('Junio'),
+('Julio'),
+('Agosto'),
+('Septiembre'),
+('Octubre'),
+('Noviembre'),
+('Diciembre')
+
+create table if not exists tipo_residencia(
+	tipo_residencia_id serial primary key,
+	residencia varchar(50) not null
+)
+
+insert into tipo_residencia (residencia)
+values
+('Hotel'),
+('Hostal'),
+('Albergue'),
+('Apartamento'),
+('Casa rural')
