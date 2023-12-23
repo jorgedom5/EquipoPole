f2f91443a31621ca8b6cb912c820976481cb4e0a
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2023-12-23 14:27:55 +0100

    Update filtros.py

--- docker/filtros.py
+++ docker/filtros.py
@@ -1,1 +1,22 @@
 #archivo para poner los filtros
+
+import pandas as pd
+import pyodbc as pdb
+import psycopg2
+import numpy as np
+import faker
+
+#CONEXIONES BASE DE DATOS
+host = "localhost" #SI ES PARA EL DOCKER-COMPOSE, CAMBIAR POR "postgres"
+database = "postgres"
+user = "root"
+password = "root"
+port = "5432"
+
+connection_target = psycopg2.connect(
+        host=host,
+        database=database,
+        user=user,
+        password=password,
+        port=port
+    )
